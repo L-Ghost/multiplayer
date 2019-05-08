6f87bf8c79fa14b3f73b2daeceb3ae01bfe74981
Luis Arantes <luis.arantes.sp@gmail.com>
2019-05-08 18:03:22 -0300

    fix rubocop offenses after changing database

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 # DATABASE
-#gem 'sqlite3'
+# gem 'sqlite3'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- spec/features/user/user_respond_event_solicitations_spec.rb
+++ spec/features/user/user_respond_event_solicitations_spec.rb
@@ -22,7 +22,9 @@ feature 'User view event solicitations' do
     user1 = create(:user)
     user2 = create(:user)
     event = create(:event, user: user1)
-    event_request = create(:event_request, event: event, user: user2, event_owner: user1)
+    event_request = create(
+      :event_request, event: event, user: user2, event_owner: user1
+    )
 
     login_as user1, scope: :user
     visit event_path(event)
@@ -38,7 +40,9 @@ feature 'User view event solicitations' do
     user1 = create(:user)
     user2 = create(:user)
     event = create(:event, user: user1)
-    event_request = create(:event_request, event: event, user: user2, event_owner: user1)
+    event_request = create(
+      :event_request, event: event, user: user2, event_owner: user1
+    )
 
     login_as user1, scope: :user
     visit event_path(event)
